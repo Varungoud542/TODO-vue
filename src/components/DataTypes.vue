0e3dab54197ed86faff0c168c3faca2094f70108
Varun <varungoud542@gmail.com>
2022-04-22 17:00:38 +0530

    Added files

---
+++ src/components/DataTypes.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "DataType",
+  data() {
+    return {
+      name: "varun",
+      numb: 1,
+      member: {
+        name: "varun",
+        age: "20",
+        color: "white",
+      },
+      familys: [
+        { name: "varun", age: 20 },
+        { name: "sucharitha", age: 28 },
+        { name: "tinku", age: 26 },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <div>String: {{ name }}</div>
+  <div>Number: {{ numb }}</div><br/>
+  <div>{{ member.name }} {{ member.age }} {{ member.color }}</div>
+  <div v-for="(family, index) in familys">
+    <div>{{ index+1 }}.{{ family.name }}-{{ family.age }}</div>
+  </div>
+</template>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,52 @@
+<script>
+
+// var vm = new Vue({
+//   data: {
+//       addNewToDo: "",
+//       todos: ["clean dishes", "wash cloths", "watch tv", "learn vue"],
+//       deleteEvent: function (index) {
+//         this.todos.splice(index, 1);
+//       },
+//       editEvent: function (item, index) {
+//         this.$set(this.todos,index,this.addNewToDo)
+
+//       },
+//       addTodos() {
+//         this.todos.push(this.addNewToDo);
+//       },
+//     };
+//   },
+// })
+export default {
+  name: "ToDo",
+  data() {
+    return {
+      addNewToDo: "",
+      todos: ["clean dishes", "wash cloths", "watch tv", "learn vue"],
+      deleteEvent: function (index) {
+        this.todos.splice(index, 1);
+      },
+      editEvent: function (item, index) {
+        Vue.set(this.todos,index,this.addNewToDo)
+
+      },
+      addTodos() {
+        this.todos.push(this.addNewToDo);
+      },
+    };
+  },
+};
+</script>
+<template>
+  <input v-model="addNewToDo" placeholder="ADD TODOS" />
+  <button @click="addTodos()">submit</button>
+  <h1 v-if="todos.length >= 1">TODO list:</h1>
+  <ul>
+    <ol v-for="(todo, index) in todos">
+      <button @click="editEvent(todo, index)">EDIT</button
+      >{{
+        todo
+      }}<button @click="deleteEvent(index)">-</button>
+    </ol>
+  </ul>
+</template>
